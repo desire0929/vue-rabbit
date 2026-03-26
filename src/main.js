83088f319a3c99150566d98a07969cef422a8c5b
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-03-26 16:23:56 +0800

    静态资源引入

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+//import './assets/main.css'
+//引入初始化样式文件
+import '@/styles/common.scss'
 
 const app = createApp(App)
 
