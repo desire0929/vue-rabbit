89e60f901decbe08ff48b1f65b4dc19572f44441
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-03-29 14:32:34 +0800

    home-人气推荐业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,6 @@ export const getBannerApi = () => httpInstance.get('/home/banner')
 
 //获取新鲜好物
 export const getNewGoodsApi = () => httpInstance.get('/home/new')
+
+//获取人气推荐
+export const getHotGoodsApi = () => httpInstance.get('/home/hot')
