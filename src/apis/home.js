ad84927cc09e8c106f6ef2b93c3aee0b150a0458
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-03-29 12:07:03 +0800

    home-新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -2,3 +2,6 @@ import httpInstance from '@/utils/http'
 
 //获取banner
 export const getBannerApi = () => httpInstance.get('/home/banner')
+
+//获取新鲜好物
+export const getNewGoodsApi = () => httpInstance.get('/home/new')
