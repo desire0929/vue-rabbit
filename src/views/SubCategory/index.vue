efc94c7e5c7caf5a75ce05c28eaf18774b82daba
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-03-30 16:41:22 +0800

    二级分类-列表筛选功能实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -34,6 +34,13 @@ const getGoodsList = async () => {
 
 onMounted(() => getGoodsList())
 
+//tab切换事件
+const tabChange =  () => {
+    console.log('tab change', reqData.value.sortField)
+    reqData.value.page = 1
+    getGoodsList()
+}
+
 </script>
 
 <template>
@@ -48,7 +55,7 @@ onMounted(() => getGoodsList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
