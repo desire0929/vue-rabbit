ebf0c0d4c337509384e629a61a982aee5e27717e
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-03-30 16:05:15 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -3,3 +3,5 @@ import httpInstance from '@/utils/http'
 
 export const getCategoryApi = (id) => httpInstance.get(`/category?id=${id}`)
 
+//获取二级分类列表数据
+export const getCategoryFilterApi = (id) => httpInstance.get(`/category/sub/filter?id=${id}`)
