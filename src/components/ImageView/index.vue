50b83649fa915455c285596e2e130db273261a0f
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-03-31 17:27:15 +0800

    详情页-图片预览组件-放大镜-滑块跟随移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { useMouseInElement } from '@vueuse/core';
 
 // 图片列表
 const imageList = [
@@ -16,6 +17,33 @@ const activeIndex = ref(0);
 const enterhandler = (i) => {
     activeIndex.value = i;
 }
+
+//2.放大镜效果
+//获取鼠标相对位置
+const target = ref(null);
+const { elementX, elementY, isOutside } = useMouseInElement(target);
+//控制滑块跟随鼠标移动（监听elementX和elementY,一旦变化，重新设置left和top）
+const left = ref(0);
+const top = ref(0);
+watch([elementX, elementY, isOutside], () => {
+    // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
+    if (isOutside.value) return;
+    //有效范围内控制滑块移动
+    //横向
+    if(elementX.value > 100 && elementX.value <300){
+        left.value = elementX.value - 100;
+    }
+    //纵向
+    if(elementY.value > 100 && elementY.value <300){
+        top.value = elementY.value - 100;
+    }
+    //处理边界
+    if(elementX.value > 300){ left.value = 200 }
+    if(elementX.value < 100){ left.value = 0 }
+
+    if(elementY.value > 300){ top.value = 200 }
+    if(elementY.value < 100){ top.value = 0 }
+})
 </script>
 
 <template>
@@ -24,7 +52,7 @@ const enterhandler = (i) => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
