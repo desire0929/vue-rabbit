fc2516787ba7a7e4375878292c15fff099d6ee5b
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-04-01 10:33:56 +0800

    详情页-通用组件统一注册全局

--- src/main.js
+++ src/main.js
@@ -3,18 +3,20 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { lazyPlugin } from './directives'
-
 //import './assets/main.css'
+
 //引入初始化样式文件
 import '@/styles/common.scss'
-
+//引入懒加载指令插件并注册
+import { lazyPlugin } from './directives'
+//引入全局组件插件并注册
+import { componentPlugin } from './components'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
-
+app.use(componentPlugin)
 app.mount('#app')
 
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { getDetailApi } from '@/apis/detail.js'
 import DetailHot from './components/DetailHot.vue';
-import ImageView from '@/components/ImageView/index.vue';
-import XtxSku from '@/components/XtxSku/index.vue';
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 
@@ -47,7 +45,7 @@ const skuChange = (sku) => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :image-list="goods.mainPictures"/>
+              <XtxImageView :image-list="goods.mainPictures"/>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
