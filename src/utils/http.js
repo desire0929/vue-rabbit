baaa13741f7884799027c6ccb94cef2e62632f94
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-04-01 17:46:53 +0800

    登录-token失效401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user.js'
+import router from '@/router'
 //axios基础的封装
 import axios from 'axios'
 
@@ -23,11 +24,19 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   //统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+  //401 token失效处理
+  //1.清除用户数据
+  //2.跳转登录页
+  if(e.response.status === 401){
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
