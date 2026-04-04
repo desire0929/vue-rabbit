7606177fe2fe682f49572a01e2c9c1bdb5a12496
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-04-04 16:29:20 +0800

    结算-提交订单

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -1,3 +1,6 @@
 import httpInstance from '@/utils/http'
 
 export const getCheckInfoApi = () => httpInstance.get('/member/order/pre')
+
+//创建订单
+export const createOrderApi = (data) => httpInstance.post('/member/order', data)
--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,6 +1,11 @@
 <script setup>
-import { getCheckInfoApi } from '@/apis/checkout';
+import { getCheckInfoApi, createOrderApi } from '@/apis/checkout';
 import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { useCartStore } from '@/stores/cartStore'
+
+const router = useRouter()
+const cartStore = useCartStore()
 
 const checkInfo = ref({})  // 订单对象
 const curAddress = ref({})  // 地址对象
@@ -26,6 +31,27 @@ const switchAddress = (item) => {
 const confirm = () => {
     curAddress.value = activeAddress.value
     showDialog.value = false
+}
+
+//创建订单
+const createOrder = async () => {
+    const res = await createOrderApi({
+        deliveryTimeType: 1,
+        payType: 1,
+        payChannel: 1,
+        buyerMessage: '',
+        goods: checkInfo.value.goods.map(item => {
+            return {
+                skuId: item.skuId,
+                count: item.count
+            }
+        }),
+        addressId: curAddress.value.id
+    })
+    const orderId = res.result.id
+    router.push(`/pay?id=${orderId}`)
+    //更新购物车
+    cartStore.updateNewList()
 }
 </script>
 
@@ -121,7 +147,7 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large" >提交订单</el-button>
+          <el-button @click="createOrder" type="primary" size="large" >提交订单</el-button>
         </div>
       </div>
     </div>
