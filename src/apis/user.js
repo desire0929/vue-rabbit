0dcc9f7b115a3498b748a134e6d6bbddb8c2981e
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-04-05 11:11:24 +0800

    会员中心-个人中心数据渲染

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,3 +1,5 @@
 import httpInstance from '@/utils/http'
 
 export const loginApi = ({account,password}) => httpInstance.post('/login', {account,password})
+
+export const getLikeListApi = ({ limit = 4 }) => httpInstance.get('/goods/relevant',{limit})
