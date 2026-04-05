89d611193f05ec4e59469346f3b35635feaaa218
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-04-05 10:49:44 +0800

    会员中心-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Individual from '@/views/Individual/index.vue'
+import UserInfo from '@/views/Individual/components/UserInfo.vue'
+import UserOrder from '@/views/Individual/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -48,6 +51,20 @@ const router = createRouter({
         {
           path: 'paycallback',
           component: PayBack
+        },
+        {
+          path: 'member',
+          component: Individual,
+          children: [
+            {
+              path: 'user',
+              component: UserInfo
+            },
+            {
+              path: 'order',
+              component: UserOrder
+            }
+          ]
         }
       ]
     },
